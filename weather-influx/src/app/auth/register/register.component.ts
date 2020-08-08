d3e4844c38ecaf5fb74f196130993f405d509656
kskoklis <33160440+kskoklis@users.noreply.github.com>
2020-08-08 20:25:37 +0300

    Fix an issue with mat-spinner when operation fails

--- weather-influx/src/app/auth/register/register.component.ts
+++ weather-influx/src/app/auth/register/register.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   isLoading = false;
-
+  private authStatusSub: Subscription;
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      authStatus => {
+        this.isLoading = false;
+      }
+    );
   }
 
   onSignup(form: NgForm){
@@ -22,5 +28,9 @@ export class RegisterComponent implements OnInit {
     }
     this.isLoading = true;
     this.authService.createUser(form.value.username, form.value.email, form.value.password);
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
