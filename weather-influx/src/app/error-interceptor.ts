d17e25f100ccd805d95b3bf0bad609f7d16eb103
kskoklis <33160440+kskoklis@users.noreply.github.com>
2020-08-09 00:05:29 +0300

    Create error-interceptor.ts

---
+++ weather-influx/src/app/error-interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ErrorComponent } from './error/error.component';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(private dialog: MatDialog) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError((error: HttpErrorResponse) => {
+                let errorMessage = 'An unknow error occured!';
+                if(error.error.message){
+                    errorMessage = error.message;
+                }
+                this.dialog.open(ErrorComponent, {data: {message: errorMessage}});
+                return throwError(error);
+            })
+        );
+    }
+}
