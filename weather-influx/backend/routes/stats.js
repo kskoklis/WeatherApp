e66a43b95525c7399b38bb82f0b01e52fc8b4be2
kskoklis <33160440+kskoklis@users.noreply.github.com>
2020-09-01 23:19:55 +0300

    Add new route in stats.js

--- weather-influx/backend/routes/stats.js
+++ weather-influx/backend/routes/stats.js
@@ -20,7 +20,7 @@ influxClient.getDatabaseNames()
 
 
 // get mean, max, min
-router.get("/:fun/:size/:id/:start/:end/:period", checkAuth, (req, res, next) => {
+router.get("/one/:fun/:size/:id/:start/:end/:period", checkAuth, (req, res, next) => {
     console.log(req.params);
     console.log(`select ` +  req.params.fun + `(` + req.params.size + `) from http where id = ` + req.params.id + ` and time > now() - ` + req.params.start + ` group by time(` + req.params.period + `)`);
     if (req.params.end == "99") { //this condition is true when one of the predifined values are clicked
@@ -42,6 +42,46 @@ router.get("/:fun/:size/:id/:start/:end/:period", checkAuth, (req, res, next) =>
         console.log(req.params);
         //select Mean(main_temp) from http where  id=734077 and time>='2020-01-06' and time<='2020-01-08' group by time(1h)
         influxClient.query(`select ` + req.params.fun + `(` + req.params.size + `) from http where id = ` + req.params.id + ` and time>='` + req.params.start + `' and time<='` + req.params.end +
+        `' group by time(` + req.params.period + `) fill(none) ; select "name", id from http where id =` + req.params.id + `  limit 1`)
+            .then(result => {
+                console.log(result);
+                res.status(200).json({
+                    message: "Data was fetched successfully!",
+                    result: result
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Failed to fetch data for the graph!"
+                });
+            });
+    }
+    
+});
+
+//get all
+router.get("/all/:size/:id/:start/:end/:period", checkAuth, (req, res, next) => {
+    console.log(req.params);
+    console.log(`select ` +  req.params.fun + `(` + req.params.size + `) from http where id = ` + req.params.id + ` and time > now() - ` + req.params.start + ` group by time(` + req.params.period + `)`);
+    if (req.params.end == "99") { //this condition is true when one of the predifined values are clicked
+        influxClient.query(`select Max(` + req.params.size + `), Mean(` + req.params.size + `), Min(` + req.params.size + `) from http where id = ` + req.params.id + ` and time > now() - ` + req.params.start + ` group by time(` + req.params.period + `) fill(none) ; select "name", id from http  where id =` + req.params.id + ` limit 1`)
+            .then(result => {
+                console.log("Result "+result);
+                res.status(200).json({
+                    message: "Data was fetched successfully!",
+                    result: result
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Failed to fetch data for the graph!"
+                });
+            });
+    } else {
+        console.log("custom");
+        console.log(req.params);
+        //select Mean(main_temp) from http where  id=734077 and time>='2020-01-06' and time<='2020-01-08' group by time(1h)
+        influxClient.query(`select  Max(` + req.params.size + `), Mean(` + req.params.size + `), Min(` + req.params.size + `) from http where id = ` + req.params.id + ` and time>='` + req.params.start + `' and time<='` + req.params.end +
         `' group by time(` + req.params.period + `) fill(none) ; select "name", id from http where id =` + req.params.id + `  limit 1`)
             .then(result => {
                 console.log(result);
