d97a41b32d6e135d4befa7e42f9b74761a0a7deb
kskoklis <33160440+kskoklis@users.noreply.github.com>
2020-10-06 00:18:56 +0300

    Fix influxdb.js route

--- weather-influx/backend/routes/influxdb.js
+++ weather-influx/backend/routes/influxdb.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const InfluxdbController = require('../controllers/influxdb');
-const conn = require('../middleware/connect-influx');
 
-router.get("/inf", conn, InfluxdbController.getThessWeatherInfo);
+router.get("/inf", InfluxdbController.getThessWeatherInfo);
 
 router.get("/inf/:id", InfluxdbController.getCityWeatherInfo);
 
